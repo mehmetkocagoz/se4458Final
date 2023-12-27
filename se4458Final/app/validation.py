39f918e25ba1a39db7a61633c89432f5d55afff8
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-27 17:00:45 +0300

    Add Azure SQL connection and update checkUser function

--- se4458Final/app/validation.py
+++ se4458Final/app/validation.py
@@ -1,3 +1,28 @@
-# I will use a basic sqlite
+from dotenv import load_dotenv
+import os
+import pyodbc
+
+load_dotenv()
+
+server = os.getenv("AZURE_SERVER")
+port = 1433
+user = os.getenv("AZURE_ID")
+password = os.getenv("AZURE_PASSWORD")
+database = 'finaldatabase'
+
+# Build connection string
+conn_str = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server},{port};DATABASE={database};UID={user};PWD={password}"
+def conn():
+    try:
+        # Create a connection
+        with pyodbc.connect(conn_str, timeout=5) as conn:
+            return conn
+
+    except pyodbc.Error as ex:
+        sqlstate = ex.args[1]
+        print(sqlstate)
+        return f"Error connecting to the database. SQLState: {sqlstate}"
+# I will use azure sql
 def checkUser(username,password):
+    conn()
     return True
