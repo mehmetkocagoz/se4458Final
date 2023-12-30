c8f348895d0c8df4ab9d850d19c5edd34743c451
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2023-12-30 16:23:40 +0300

    Add blood request functionality to blooddb.py

--- se4458Final/app/blooddb.py
+++ se4458Final/app/blooddb.py
@@ -46,3 +46,58 @@ def createDonorInDatabase(donor_name, blood_type, city, town, email, phone):
     connection.commit()
     connection.close()
     return "Donor Created Succesfully"
+
+def requestBloodFromDatabase(requestor, blood_type, city, town, email, units, duration):
+    connection = conn()
+    cursor = connection.cursor()
+    # First we will check BloodDonations table, if there is enough blood we will directly send email to requestor 'Blood Found Donor= {'donor_name'}'
+    cursor.execute("""
+            SELECT SUM(units) FROM BloodDonations WHERE blood_type = ?
+        """, (blood_type,))
+    total_units_available = cursor.fetchone()[0] or 0
+    # If there is enough available units, we will try to collect blood from donors
+    # It can be one or more donors therefore I used while loop inside this if clause
+    if total_units_available >= units:    
+        cursor.execute("""
+                    SELECT * FROM BloodDonations WHERE blood_type = ?
+                       """,(blood_type,))
+        donors = cursor.fetchall()
+        blood_need = units
+        i=0
+        donor_name_list = []
+        while (blood_need>0):
+            don_ID = donors[i][0]
+            donor_name = donors[i][1]
+            blood_type = donors[i][2]
+            unit = donors[i][3]
+            i = i+1
+            # If donor donated more then requested units of blood, we will update donor's unit and set the blood_need to 0, loop will end
+            if unit > blood_need:
+                unit -=blood_need
+                blood_need = 0
+                cursor.execute("""
+                            UPDATE BloodDonations SET units = ? WHERE donation_id = ?;
+                               """,(unit,don_ID,))
+                donor_name_list.append(donor_name)
+            # If donor donated equal request, we will delete donor row and set the blood_need to 0, loop will end
+            elif unit == blood_need:
+                blood_need = 0
+                cursor.execute("""
+                            DELETE FROM BloodDonations WHERE donation_id = ?
+                               """,(don_ID,))               
+                donor_name_list.append(donor_name)
+            # Else we will try to collect requested blood units from donors, loop will continue
+            else:
+                blood_need = blood_need - unit
+                cursor.execute("""
+                            DELETE FROM BloodDonations WHERE donation_id = ?
+                               """,(don_ID,))
+                donor_name_list.append(donor_name)
+        connection.commit()
+    # Else there is no enough blood, we will send a message to queue, queue will handled with another service
+    else:
+        return "Not enough blood, it sends to queue"
+            
+    connection.commit()
+    connection.close()
+    return donor_name_list
